30c8ea0ce0efc6ea6361cbfc4712149c101a673e
NadaAlfowey <101261526+NadaAlfowey@users.noreply.github.com>
2023-11-08 01:09:05 +0200

    responsive navbar edited using usestate

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import './navbar.css'
 import logo from './pmtLogo.jpg'
 import {Link} from 'react-router-dom'
+
 function Navbar() {
+  const [menuToggle, setIsMenuOpen] = useState(false);
+
+  const toggleMenu=()=>{
+    setIsMenuOpen(!menuToggle);
+  };
+
   return (
     <div>
       <nav className="navbar">
@@ -14,16 +21,16 @@ function Navbar() {
             height="40"
           />
           {/* Responsive menu Icon  */}
-          <input type="checkbox" id="toggle-box" />
+          <input type="checkbox" id="toggle-box" checked={menuToggle} onChange={toggleMenu}/>
           <label for="toggle-box" className="menu-toggle">
             <span></span>
           </label>
           {/* Navbar options  */}
           <div className="navoptions">
-            <a><Link to='/'>Home</Link></a>
-            <a><Link to='/table'>Table</Link></a>
-            <a><Link to='/aboutus'>About Us</Link></a>
-            <a><Link to='/logout'>Logout</Link></a>
+            <a><Link to='/' onClick={toggleMenu}>Home</Link></a>
+            <a><Link to='/table' onClick={toggleMenu}>Table</Link></a>
+            <a><Link to='/aboutus' onClick={toggleMenu}>About Us</Link></a>
+            <a><Link to='/logout' onClick={toggleMenu}>Logout</Link></a>
           </div>
         </div>
       </nav>
