23c6649296af94a05a62970037585bb367d39684
NadaAlfowey <101261526+NadaAlfowey@users.noreply.github.com>
2023-11-07 23:15:03 +0200

    fixated footer

--- src/components/aboutUs/aboutus.js
+++ src/components/aboutUs/aboutus.js
@@ -8,7 +8,7 @@ function AboutUs() {
         <h1>About Us</h1>
       </header>
 
-      {/* <div className="container">
+      <div className="container">
         <h2>Our Story</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consectetur tortor id nisl sollicitudin, id hendrerit risus aliquet. Suspendisse tristique, leo eget gravida dignissim, tellus eros volutpat urna, at dictum mi quam in justo.</p>
 
@@ -17,7 +17,7 @@ function AboutUs() {
 
         <h2>Meet the Team</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam fugiat, illo quia quam inventore sed alias ex corrupti quis illum assumenda ea, aliquam molestiae soluta obcaecati libero nam? Ullam, ipsam.</p>
-      </div> */}
+      </div>
     </div>
   )
 }
